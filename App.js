e16922c85ec272436264b619dbdd674176861878
Choaib Elmadi <111356633+Choaib-ELMADI@users.noreply.github.com>
2022-12-02 20:18:32 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const initialProducts = [
+  { id: 0, name: "Monkey Nft", count: 1 },
+  { id: 1, name: "The ball Nft", count: 5 },
+  { id: 2, name: "Suzaro Nft", count: 2 }
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return { ...product, count: product.count + 1 };
+        } else {
+          return product;
+        }
+      })
+    );
+  }
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
